e57b4fbd5bebdf28214ecda6d40e7d7d31474c83
KacperMitkowski <kacper.mitkowski@gmail.com>
2021-01-19 20:59:43 +0100

    Bootstrap footer added

--- BooksApp/Scripts/src/components/footer.js
+++ BooksApp/Scripts/src/components/footer.js
@@ -3,7 +3,18 @@
 export default class Footer extends Component {
     render() {
         return (
-            <h1>I am footer</h1>
+            <footer>
+                <div class="card">
+                    <div class="card-header">
+                        Kontakt
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Lorem ipsum dolor sit amet</h5>
+                        <p class="card-text">Fusce at est quis eros euismod pharetra. Fusce sed sem iaculis, luctus erat in, varius libero. Phasellus non nulla et nunc mollis tincidunt. Donec eget neque nisi.</p>
+                        <a href="#" class="btn btn-primary">Kontakt</a>
+                    </div>
+                </div>
+            </footer>
         );
     }
 }
