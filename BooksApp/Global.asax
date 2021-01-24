e6a0cb8c4d3bb4f328e16e58be3da7c0aae70944
KacperMitkowski <kacper.mitkowski@gmail.com>
2021-01-24 22:11:18 +0100

    Refactor + sql script added

--- BooksApp/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BooksApp.MvcApplication" Language="C#" %>
--- BooksApp/Scripts/src/index.js
+++
@@ -1,7 +0,0 @@
-﻿import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../src/components/app.js';
-import '../static/css/main.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-ReactDOM.render(<App />, document.getElementById("root"));
